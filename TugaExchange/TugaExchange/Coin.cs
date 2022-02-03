64fc43581d95116065e3e9baad5d6ae962b1d23b
Juka <joana.msfl.freitas@gmail.com>
2022-02-03 17:39:26 +0000

    1versao

---
+++ TugaExchange/TugaExchange/Coin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugaExchange
+{
+    class Coin
+    {
+
+        public Coin()
+        {
+
+        }
+
+        public static void AddCoin()
+        {
+            List<Coin> totalCoins = new List<Coin>();
+
+            while (true)
+            {
+                Console.WriteLine("Insira a nova moeda:");
+                var newCoin= Console.ReadLine();
+
+                //cão de guarda!
+                if (newCoin == "0")
+                    break;
+
+                totalCoins.Add(newCoin.);
+            }
+
+            if (!totalCoins.Any())
+            {
+                Console.WriteLine("Erro: lista vazia");
+            }
+
+
+        }
+    }
+
+}
---
+++ TugaExchange/TugaExchange/Menu.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TugaExchange
+{
+    internal class Menu
+    {
+        public static void Initialize()
+        {
+            List<string> menuPrincipal = new List<string>()
+            {
+                "Investidor",
+                "Administrador"
+            };
+
+
+            List<string> menuInvestidor = new List<string>()
+            {
+                "Depositar",
+                "Comprar Moeda",
+                "Vender Moeda",
+                "Mostrar Portfólio",
+                "Mostrar Câmbio",
+                "Sair"
+            };
+
+            List<string> menuAdministrador = new List<string>()
+            {
+                "Adicionar Moeda",
+                "Remover Moeda",
+                "Sair"
+            };
+
+            for (int i = 0; i < menuPrincipal.Count; i++)
+            {
+                Console.WriteLine(menuPrincipal[i]);
+            }
+
+            Console.WriteLine("Insira a opção:");
+            var opcao = Console.ReadLine();
+
+            Console.Clear();
+
+            if (opcao == "1")
+            {
+                for (int i = 0; i < menuInvestidor.Count; i++)
+                {
+                    Console.WriteLine(menuInvestidor[i]);
+                }
+
+                Console.WriteLine("Insira a operação:");
+                var opcaoInvestidor = Console.ReadLine();
+
+                switch (opcaoInvestidor)
+                {
+                    case "1": //Depositar 
+                        break;
+                    case "2": // Comprar moeda 
+                        break;
+                    case "3": // vender moeda 
+                        break;
+                    case "4": // mostrar portfólio 
+                        break;
+                    case "5": // mostrar câmbio 
+                        break;
+                    default: // sair 
+                        break;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < menuAdministrador.Count; i++)
+                {
+                    Console.WriteLine(menuAdministrador[i]);
+                }
+
+                Console.WriteLine("Insira a operação:");
+                var opcaoAdministrador = Console.ReadLine();
+
+                switch (opcaoAdministrador)
+                {
+                    case "1": // Adicionar moeda 
+                        break;
+                    case "2": // Remover moeda 
+                        break;
+                    default: // sair 
+                        break;
+                }
+            }
+        }
+    }
+}
